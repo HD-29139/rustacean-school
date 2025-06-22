055c7dec435eb9fc33662f2a109859998caf7d08
HD-29139 <santanajefferson594@gmail.com>
2025-06-22 03:43:19 -0300

    Controllers: Aluno

---
+++ Controllers/AlunoController.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using escola.Models;
+
+namespace escola.Controllers;
+
+public class AlunoController : Controller
+{
+    private readonly ILogger<AlunoController> _logger;
+
+    public AlunoController(ILogger<AlunoController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
