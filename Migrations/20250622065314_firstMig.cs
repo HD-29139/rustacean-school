6da64fabef0970877c09a118309fe247dc8efd4e
HD-29139 <santanajefferson594@gmail.com>
2025-06-22 05:02:57 -0300

    migrations

---
+++ Migrations/20250622065314_firstMig.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace escola.Migrations
+{
+    /// <inheritdoc />
+    public partial class firstMig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Alunos",
+                columns: table => new
+                {
+                    AlunoID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alunos", x => x.AlunoID);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Disciplinas",
+                columns: table => new
+                {
+                    DisciplinaID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Disciplinas", x => x.DisciplinaID);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Matriculas",
+                columns: table => new
+                {
+                    MatriculaID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    AlunoID = table.Column<int>(type: "int", nullable: false),
+                    DisciplinaID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Matriculas", x => x.MatriculaID);
+                    table.ForeignKey(
+                        name: "FK_Matriculas_Alunos_AlunoID",
+                        column: x => x.AlunoID,
+                        principalTable: "Alunos",
+                        principalColumn: "AlunoID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Matriculas_Disciplinas_DisciplinaID",
+                        column: x => x.DisciplinaID,
+                        principalTable: "Disciplinas",
+                        principalColumn: "DisciplinaID",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Notas",
+                columns: table => new
+                {
+                    NotaID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Valor = table.Column<double>(type: "double", nullable: false),
+                    MatriculaID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notas", x => x.NotaID);
+                    table.ForeignKey(
+                        name: "FK_Notas_Matriculas_MatriculaID",
+                        column: x => x.MatriculaID,
+                        principalTable: "Matriculas",
+                        principalColumn: "MatriculaID",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matriculas_AlunoID",
+                table: "Matriculas",
+                column: "AlunoID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matriculas_DisciplinaID",
+                table: "Matriculas",
+                column: "DisciplinaID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notas_MatriculaID",
+                table: "Notas",
+                column: "MatriculaID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notas");
+
+            migrationBuilder.DropTable(
+                name: "Matriculas");
+
+            migrationBuilder.DropTable(
+                name: "Alunos");
+
+            migrationBuilder.DropTable(
+                name: "Disciplinas");
+        }
+    }
+}
