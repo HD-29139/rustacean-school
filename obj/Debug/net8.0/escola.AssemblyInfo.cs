d43c28bc3c2de1d0bd708fa70e4fbc12be1dd6db
HD-29139 <santanajefferson594@gmail.com>
2025-06-22 03:07:17 -0300

    debug

--- obj/Debug/net8.0/escola.AssemblyInfo.cs
+++ obj/Debug/net8.0/escola.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("escola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0acd49091bf87fe6dd5e60b9dce6792f6847bc4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("escola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("escola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
