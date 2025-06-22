cf3301012b4ef121218809f15a09025b7bdbab58
HD-29139 <santanajefferson594@gmail.com>
2025-06-22 05:02:38 -0300

    AlunoController: Create

--- Controllers/AlunoController.cs
+++ Controllers/AlunoController.cs
@@ -1,21 +1,34 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using escola.Models;
+using escola.Data;
+using escola.Repository;
 
 namespace escola.Controllers;
 
 public class AlunoController : Controller
 {
-    private readonly ILogger<AlunoController> _logger;
+    private readonly IAlunoRepository alunoRepository;
 
-    public AlunoController(ILogger<AlunoController> logger)
+    public AlunoController(IAlunoRepository AlunoRepository)
     {
-        _logger = logger;
+        alunoRepository = AlunoRepository;
     }
-
     public IActionResult Index()
     {
         return View();
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Aluno aluno)
+    {
+        alunoRepository.Create(aluno);
+        return RedirectToAction("Index");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
