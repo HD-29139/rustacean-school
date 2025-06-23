176c1135da5f3b08402fab3cb6f37b79ec0de8b7
HD-29139 <santanajefferson594@gmail.com>
2025-06-22 21:43:41 -0300

    implementation: interface methods

--- Repository/AlunoRepository.cs
+++ Repository/AlunoRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using escola.Data;
 using escola.Models;
+using Microsoft.Identity.Client;
 
 namespace escola.Repository
 {
@@ -15,12 +16,31 @@ namespace escola.Repository
         {
             this.bancoContext = bancoContext;
         }
-        
+
         public Aluno Create(Aluno aluno)
         {
             bancoContext.Alunos.Add(aluno);
             bancoContext.SaveChanges();
             return aluno;
+        }
+
+        public Aluno IDfinder(int id)
+        {
+            return bancoContext.Alunos.FirstOrDefault(x => x.AlunoID == id);
+        }
+
+        public List<Aluno> listaralunos()
+        {
+            return bancoContext.Alunos.ToList();
+        }
+
+        public Aluno EditarAluno(Aluno aluno)
+        {
+            Aluno alunoEdit = IDfinder(aluno.AlunoID) ?? throw new Exception("Aluno não encontrado");
+            alunoEdit.Nome = aluno.Nome;
+            bancoContext.Alunos.Update(alunoEdit);
+            bancoContext.SaveChanges();
+            return alunoEdit;
         }
     }
 }
