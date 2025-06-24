d205dc3db2cbf111400a6a958e71187e006f5332
HD-29139 <santanajefferson594@gmail.com>
2025-06-23 21:32:38 -0300

    MatriculaController: Create/List/IdFinder/Update/Remove

--- Controllers/MatriculaController.cs
+++ Controllers/MatriculaController.cs
@@ -1,21 +1,65 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using escola.Models;
+using escola.Data;
+using escola.Repository;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 
 namespace escola.Controllers;
 
 public class MatriculaController : Controller
 {
-    private readonly ILogger<MatriculaController> _logger;
+    private readonly IMatriculaRepository MatriculaRepository;
+    private readonly BancoContext BancoContext;
 
-    public MatriculaController(ILogger<MatriculaController> logger)
+    public MatriculaController(IMatriculaRepository matriculaRepository, BancoContext bancoContext)
     {
-        _logger = logger;
+        MatriculaRepository = matriculaRepository;
+        BancoContext = bancoContext;
     }
-
     public IActionResult Index()
     {
+        List<Matricula> matricula = MatriculaRepository.listarmatriculas();
+        return View(matricula);
+    }
+
+    public IActionResult Create()
+    {
+        ViewBag.Alunos = new SelectList(BancoContext.Alunos, "AlunoID", "Nome");
+        ViewBag.Disciplinas = new SelectList(BancoContext.Disciplinas, "DisciplinaID", "Nome");
         return View();
+    }
+    public IActionResult Edit(int id)
+    {
+        Matricula matricula = MatriculaRepository.IDfinder(id);
+        ViewBag.Alunos = new SelectList(BancoContext.Alunos, "AlunoID", "Nome");
+        ViewBag.Disciplinas = new SelectList(BancoContext.Disciplinas, "DisciplinaID", "Nome");
+        return View(matricula);
+    }
+    public IActionResult Delete(int id)
+    {
+        Matricula matricula = MatriculaRepository.IDfinder(id);
+        return View(matricula);
+    }
+    public IActionResult Remove(int id)
+    {
+        MatriculaRepository.Remove(id);
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public IActionResult Update(Matricula matricula)
+    {
+            MatriculaRepository.EditarMatricula(matricula);
+            return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public IActionResult Create(Matricula matricula)
+    {
+        MatriculaRepository.Create(matricula);
+        return RedirectToAction("Index");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
