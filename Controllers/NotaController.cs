d9e1c9539e4fa12d1fa7cc2e80e66838c7fb06ac
HD-29139 <santanajefferson594@gmail.com>
2025-06-25 01:44:33 -0300

    NotaController: Create/List/IdFinder/Update/Remove

--- Controllers/NotaController.cs
+++ Controllers/NotaController.cs
@@ -1,21 +1,63 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using escola.Models;
+using escola.Data;
+using escola.Repository;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 
 namespace escola.Controllers;
 
 public class NotaController : Controller
 {
-    private readonly ILogger<NotaController> _logger;
+    private readonly INotaRepository NotaRepository;
+    private readonly BancoContext BancoContext;
 
-    public NotaController(ILogger<NotaController> logger)
+    public NotaController(INotaRepository notaRepository, BancoContext bancoContext)
     {
-        _logger = logger;
+        NotaRepository = notaRepository;
+        BancoContext = bancoContext;
     }
-
     public IActionResult Index()
     {
+        List<Nota> nota = NotaRepository.listarnotas();
+        return View(nota);
+    }
+
+    public IActionResult Create()
+    {
+        ViewBag.Matriculas = new SelectList(BancoContext.Matriculas, "MatriculaID", "MatriculaID");
         return View();
+    }
+    public IActionResult Edit(int id)
+    {
+        Nota nota = NotaRepository.IDfinder(id);
+        ViewBag.Matriculas = new SelectList(BancoContext.Matriculas, "MatriculaID", "MatriculaID");
+        return View(nota);
+    }
+    public IActionResult Delete(int id)
+    {
+        Nota nota = NotaRepository.IDfinder(id);
+        return View(nota);
+    }
+    public IActionResult Remove(int id)
+    {
+        NotaRepository.Remove(id);
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public IActionResult Update(Nota nota)
+    {
+            NotaRepository.EditarNota(nota);
+            return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public IActionResult Create(Nota nota)
+    {
+        NotaRepository.Create(nota);
+        return RedirectToAction("Index");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
